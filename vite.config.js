67a923aa5173aca1e88e9c88bc25103e6ac594ef
Arun. <arun@raincluster.com>
2026-07-20 16:44:33 +0530

    Deploy to GitHub Pages and Netlify concurrently

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { ytProxyPlugin } from './vite-plugin-yt-proxy.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     ytProxyPlugin(),
